dde68d29cdc0e26673e2f752ce51c6178c557060
Mikołaj Jędrzejewski <mikolajedjed@gmail.com>
2022-01-18 00:53:41 +0100

    Delete src directory

--- src/evaluation.h
+++
@@ -1,7 +0,0 @@
-#ifndef EVAL
-#define EVAL
-// looks at score of every player and decides which one should win
-// arguments: array of players, number of players
-// returns: id of the winner
-int determine_winner(struct player* players, int n);
-#endif
